60871657920ce9af716daa77d524ef8c36b5a547
Noel Delgado <noeldelgadom@myhost.local>
2017-08-29 10:59:28 -0500

    home and toolbar components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MaterializeModule } from 'angular2-materialize';
 import { AppComponent } from './app.component';
+import { Angular2TokenService } from 'angular2-token';
+import { HomeComponent } from './home/home.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
+    AppRoutingModule,
+    MaterializeModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ Angular2TokenService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
